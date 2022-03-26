93cbde5532fa3f86ecf06b6074d2d088ecec1ec4
pedroigorreis <pedro.igor@sga.pucminas.br>
2022-03-25 23:01:41 -0300

    Correção 01.1

--- Arquivos/Conta.java
+++ Arquivos/Conta.java
@@ -6,106 +6,72 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class Conta
+public class Conta 
 {
-	private int idConta;
-    private Usuario usuario;
-    private float saldo = 0F;
-    private static int contadorContas = 1;
-    private short transferenciasRealizadas = 0;
-
-    public Conta (Usuario usuario)
-    {
-        this.usuario = usuario;
-        this.idConta = contadorContas;
-        contadorContas += 1;
-    }
-
-    public float getSaldo() { return saldo; }
-    public int getIdConta() { return idConta; } 
-    public Usuario getUsuario() { return usuario; }
-    public short getTransferenciasRealizadas() { return transferenciasRealizadas; }
-
-    public void setSaldo( float saldo ) { this.saldo = saldo; }
-    public void setIdConta( int idConta ) { this.idConta = idConta; }
-    public void setUsuario( Usuario usuario ) { this.usuario = usuario; }
-    public void setTR( short transferenciasRealizadas ) { this.transferenciasRealizadas = transferenciasRealizadas; }
-
+	protected String cpf;
+	protected int idConta;
+	protected String cidade;
+	protected float saldo = 0F;
+	protected String nomePessoa;
+	protected short transferenciasRealizadas = 0;
+	
+	public Conta() // Conta genérica para preenchimento.
+	{
+		this.cpf = "";
+		this.saldo = 0F;
+		this.cidade = "";
+		this.idConta = -1;
+		this.nomePessoa = "";
+	}
+	
+	public Conta(String cpf, String cidade, float saldo, String nomePessoa) // Conta com os dados providos pelo usuário. 
+	{
+		this.cpf = cpf;
+		this.saldo = saldo;
+		this.cidade = cidade;
+		this.idConta = (int)Math.floor(Math.random()*(9999-1000+1)+1000);
+		this.nomePessoa = nomePessoa;
+		this.transferenciasRealizadas = 0;
+	}
+	
     public String toString()
     {
-        return "\n\tNúmero da conta: " + this.getIdConta() 
-        		+ "\n\tNome completo: " + this.usuario.getNome()
-        		+ "\n\tCidade: " + this.usuario.getCidade()
-        		+ "\n\tCPF: " + this.usuario.getCpf() 
-        		+ "\n\tSaldo: " + ConversorReal.floatParaString(this.getSaldo()) + "\n"
-        		+ "\n\tN° de transferências: " + this.getTransferenciasRealizadas();
-    }
-
-    public void depositar(float valorSaldo)
-    {
-        if(valorSaldo > 0)
-        {
-            setSaldo(getSaldo() + valorSaldo);
-            System.out.printf("\tDepósito realizado com sucesso.\n");
-        }
-        else
-        {
-            System.out.printf("\tSaldo insuficiente para esta operação.\n");
-        }
-    }
-
-    public void sacar (float valorSaldo)
-    {
-        if(valorSaldo >= 0 && this.getSaldo() >= valorSaldo)
-        {
-            setSaldo(getSaldo() - valorSaldo);
-            System.out.printf("\tSaque realizado com sucesso.\n");
-        }
-        else
-        {
-            System.out.printf("\tSaldo insuficiente para esta operação.\n");
-        }
-    }
-
-    public void transferir (Conta contaDestinaria, float valorSaldo)
-    {
-        if(valorSaldo > 0 && this.getSaldo() >= valorSaldo)
-        {
-            setSaldo(getSaldo() - valorSaldo);
-            contaDestinaria.saldo = contaDestinaria.getSaldo() + valorSaldo;
-            System.out.printf("\tTransferência realizada com sucesso.\n");
-        }
-        else
-        {
-            System.out.printf("\tOps! Ocorreu um erro ao realizar a operação.\n");
-        }
+        	return "\n\t.-----------------------. "
+        	 + "\n\t| Número da conta       | " + idConta
+        	 + "\n\t| Nome completo         | " + nomePessoa
+        	 + "\n\t| Cidade                | " + cidade
+        	 + "\n\t| CPF                   | " + cpf 
+        	 + "\n\t| Saldo                 | " + Conversor.floatParaString(saldo)
+        	 + "\n\t| N° de transferências  | " + transferenciasRealizadas
+        	 + "\n\t'-----------------------' "
+        	 + "\n";
     }
     
-    public void fromByteArray(byte[] dados) throws IOException
-	{
-		 ByteArrayInputStream leitor = new ByteArrayInputStream(dados);
-		 DataInputStream DIS = new DataInputStream(leitor);
-		 
-		 idConta = DIS.readInt();
-		 usuario.setNome(DIS.readUTF());
-		 usuario.setCpf(DIS.readUTF());
-		 usuario.setCidade(DIS.readUTF());
-		 saldo = DIS.readFloat();
-		 transferenciasRealizadas = DIS.readShort();
-	}
+    public byte[] toByteArray() throws IOException
+    {
+    	ByteArrayOutputStream gerador = new ByteArrayOutputStream();
+    	DataOutputStream DOS = new DataOutputStream(gerador);
+
+        DOS.writeInt(idConta);
+        DOS.writeUTF(nomePessoa);
+        DOS.writeUTF(cpf);
+        DOS.writeUTF(cidade);
+        DOS.writeShort(transferenciasRealizadas);
+        DOS.writeFloat(saldo);
+        
+        return gerador.toByteArray();
+    }
     
-	public byte[] toByteArray() throws IOException 
-	{
-		ByteArrayOutputStream gerador = new ByteArrayOutputStream();
-		DataOutputStream DOS = new DataOutputStream(gerador);
-		
-		DOS.writeInt(getIdConta());
-		DOS.writeUTF(getUsuario().getNome());
-		DOS.writeUTF(getUsuario().getCidade());
-		DOS.writeUTF(getUsuario().getCpf());
-		DOS.writeFloat(getSaldo());
-		DOS.writeShort(getTransferenciasRealizadas());
-		
-		return gerador.toByteArray();
-	}
+    public void fromByteArray(byte[] byteContas) throws IOException
+    {
+    	ByteArrayInputStream leitor = new ByteArrayInputStream(byteContas);
+    	DataInputStream DIS = new DataInputStream(leitor);
+    	
+    	idConta = DIS.readInt();
+    	nomePessoa = DIS.readUTF();
+    	cpf = DIS.readUTF();
+    	cidade = DIS.readUTF();
+    	transferenciasRealizadas = DIS.readShort();
+    	saldo = DIS.readFloat();
+    }
 }
--- Arquivos/Menus.java
+++ Arquivos/Menus.java
@@ -4,84 +4,54 @@ public class Menus
 {
 	public static void menuInicial()
 	{
-        System.out.printf("\n");
-        System.out.printf("\t.---------------------------------------.\n");
-        System.out.printf("\t|            No bolso! | ADM            |\n");
-        System.out.printf("\t|---------------------------------------|\n");
-        System.out.printf("\t|       » Escolha uma das opções «      |\n");
-        System.out.printf("\t|---------------------------------------|\n");
-        System.out.printf("\t| 1 - Criar uma nova conta              |\n");
-        System.out.printf("\t| 2 - Depositar                         |\n");
-        System.out.printf("\t| 3 - Sacar                             |\n");
-        System.out.printf("\t| 4 - Transferir                        |\n");
-        System.out.printf("\t| 5 - Listar contas                     |\n");
-        System.out.printf("\t| 6 - Atualizar conta                   |\n");
-        System.out.printf("\t| 7 - Deletar conta                     |\n");
-        System.out.printf("\t| 8 - Sair                              |\n");
-        System.out.printf("\t'---------------------------------------'\n");
-        System.out.printf("\n");
+		System.out.printf("\n");
+		System.out.printf("\t.---------------------------------------.\n");
+		System.out.printf("\t|            No bolso! | ADM            |\n");
+		System.out.printf("\t|---------------------------------------|\n");
+		System.out.printf("\t|       » Escolha uma das opções «      |\n");
+		System.out.printf("\t|---------------------------------------|\n");
+		System.out.printf("\t| 1 - Criar uma nova conta              |\n");
+		System.out.printf("\t| 2 - Atualizar conta                   |\n");
+		System.out.printf("\t| 3 - Transferir                        |\n");
+		System.out.printf("\t| 4 - Deletar conta                     |\n");
+		System.out.printf("\t| 5 - Sair                              |\n");
+		System.out.printf("\t'---------------------------------------'\n");
+		System.out.printf("\n");
 	}
 	
 	public static void menuNovaConta()
 	{
 		System.out.printf("\n");
-        System.out.printf("\t.----------------------.\n");
-        System.out.printf("\t|      Nova Conta      |\n");
-        System.out.printf("\t'----------------------'\n");
-        System.out.printf("\n");
-	}
-	
-	public static void menuDeposito()
-	{
+		System.out.printf("\t.----------------------.\n");
+		System.out.printf("\t|      Nova Conta      |\n");
+		System.out.printf("\t'----------------------'\n");
 		System.out.printf("\n");
-        System.out.printf("\t.----------------------.\n");
-        System.out.printf("\t|       Depósito       |\n");
-        System.out.printf("\t'----------------------'\n");
-        System.out.printf("\n");
 	}
-	
-	public static void menuSaque()
-	{
-		System.out.printf("\n");
-        System.out.printf("\t.---------------.\n");
-        System.out.printf("\t|     Saque     |\n");
-        System.out.printf("\t'---------------'\n");
-        System.out.printf("\n");
-	}
-	
+		
 	public static void menuTransferencia()
 	{
 		System.out.printf("\n");
-        System.out.printf("\t.---------------------------.\n");
-        System.out.printf("\t|       Transferência       |\n");
-        System.out.printf("\t'---------------------------'\n");
-        System.out.printf("\n");
+		System.out.printf("\t.---------------------------.\n");
+		System.out.printf("\t|       Transferência       |\n");
+		System.out.printf("\t'---------------------------'\n");
+		System.out.printf("\n");
 	}
 	
 	public static void menuAtualizacao()
 	{
 		System.out.printf("\n");
-        System.out.printf("\t.---------------------------.\n");
-        System.out.printf("\t|      Atualizar conta      |\n");
-        System.out.printf("\t'---------------------------'\n");
-        System.out.printf("\n");
+		System.out.printf("\t.---------------------------.\n");
+		System.out.printf("\t|      Atualizar conta      |\n");
+		System.out.printf("\t'---------------------------'\n");
+		System.out.printf("\n");
 	}
 	
 	public static void menuDeletarConta()
 	{
 		System.out.printf("\n");
-        System.out.printf("\t.---------------------------.\n");
-        System.out.printf("\t|       Deletar conta       |\n");
-        System.out.printf("\t'---------------------------'\n");
-        System.out.printf("\n");
-	}
-	
-	public static void menuListarConta()
-	{
+		System.out.printf("\t.---------------------------.\n");
+		System.out.printf("\t|       Deletar conta       |\n");
+		System.out.printf("\t'---------------------------'\n");
 		System.out.printf("\n");
-        System.out.printf("\t.------------------------.\n");
-        System.out.printf("\t|      Listar conta      |\n");
-        System.out.printf("\t'------------------------'\n");
-        System.out.printf("\n");
 	}
 }
